9520f9a45384da8384583b08bcd40c0af60dc3c2
tataadin <tataa2237@gmail.com>
2022-02-08 21:14:58 +0700

    [depedencies] implement getX

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/modules/home/view.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Todo List',
+      home: HomePage(),
+    );
+  }
+}
