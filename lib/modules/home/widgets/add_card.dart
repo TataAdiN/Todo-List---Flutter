9a99000b26bc8dd26ebfcef17110767ca75177ff
tataadin <tataa2237@gmail.com>
2022-03-02 21:56:33 +0700

    [add_card] show dialog onTap

--- lib/modules/home/widgets/add_card.dart
+++ lib/modules/home/widgets/add_card.dart
@@ -18,7 +18,13 @@ class AddCard extends StatelessWidget {
       height: squareWidth / 2,
       margin: EdgeInsets.all(3.0.wp),
       child: InkWell(
-        onTap: () {},
+        onTap: () async {
+          await Get.defaultDialog(
+            titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
+            radius: 5,
+            title: 'Task Type'
+          );
+        },
         child: DottedBorder(
           color: Colors.grey[400]!,
           dashPattern: const [8,4],
