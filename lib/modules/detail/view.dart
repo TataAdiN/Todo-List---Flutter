1acc681c2622d1b4d83eb7cf0dbe5c4716fcbfd3
tataadin <tataa2237@gmail.com>
2022-05-28 12:26:35 +0700

    [todos] fix create todos

--- lib/modules/detail/view.dart
+++ lib/modules/detail/view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:notes_app/core/utils/extensions.dart';
 import 'package:notes_app/modules/home/controller.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class DetailPage extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
@@ -12,7 +14,9 @@ class DetailPage extends StatelessWidget {
     var task = homeCtrl.task.value!;
     var color = HexColor.fromHex(task.color);
     return Scaffold(
-      body: ListView(
+        body: Form(
+      key: homeCtrl.formKey,
+      child: ListView(
         children: [
           Padding(
             padding: EdgeInsets.all(3.0.wp),
@@ -20,6 +24,8 @@ class DetailPage extends StatelessWidget {
               children: [
                 IconButton(
                     onPressed: () {
+                      homeCtrl.formCtrl.clear();
+                      homeCtrl.updateTodo();
                       homeCtrl.changeTask(null);
                       Get.back();
                     },
@@ -40,9 +46,76 @@ class DetailPage extends StatelessWidget {
                         fontSize: 12.0.sp, fontWeight: FontWeight.bold),
                   )
                 ],
-              ))
+              )),
+          Obx(() {
+            var totalTodos =
+                homeCtrl.doingTodos.length + homeCtrl.doneTodos.length;
+            return Padding(
+                padding:
+                    EdgeInsets.only(left: 16.0.wp, right: 16.0.wp, top: 3.0.wp),
+                child: Row(
+                  children: [
+                    Text(
+                      "$totalTodos Tasks",
+                      style: TextStyle(fontSize: 12.0.sp, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      width: 3.0.wp,
+                    ),
+                    Expanded(
+                        child: StepProgressIndicator(
+                      totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                      currentStep: homeCtrl.doneTodos.length,
+                      size: 5,
+                      padding: 0,
+                      selectedGradientColor: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [color.withOpacity(0.5), color]),
+                      unselectedGradientColor: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Colors.grey[300]!, Colors.grey[300]!]),
+                    ))
+                  ],
+                ));
+          }),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.0.wp, vertical: 2.0.wp),
+            child: TextFormField(
+              controller: homeCtrl.formCtrl,
+              autofocus: true,
+              decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[400]!)),
+                  prefixIcon: Icon(
+                    Icons.check_box_outline_blank,
+                    color: Colors.grey[400],
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      if(homeCtrl.formKey.currentState!.validate()){
+                        var result = homeCtrl.addTodo(homeCtrl.formCtrl.text);
+                        if(result){
+                          EasyLoading.showSuccess("Todo item add success");
+                        }else{
+                          EasyLoading.showError("Todo item is already exist");
+                        }
+                        homeCtrl.formCtrl.clear();
+                      }
+                    },
+                    icon: const Icon(Icons.done),
+                  )),
+              validator: (value){
+                if(value == null || value.trim().isEmpty){
+                  return "Please enter your todo item";
+                }
+                return null;
+              },
+            ),
+          )
         ],
       ),
-    );
+    ));
   }
 }
